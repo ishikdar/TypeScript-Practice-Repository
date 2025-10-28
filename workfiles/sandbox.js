a945c16d5a688e4e97de684253879ddd00b8ffb4
Md Al Imran Shikdar <mdalimranshikdar@Mds-MacBook-Pro.local>
2025-10-28 15:12:56 +0600

    dynamic any types

--- workfiles/sandbox.js
+++ workfiles/sandbox.js
@@ -1,35 +1,24 @@
-//explicit types
-var character;
-var age;
-var isLoggedIn;
-//age = 'luigi' // error
-age = 30;
-//character = 20; // error
-character = 'mario';
-//isLoggedIn = 25; // error
-isLoggedIn = true;
-//arrays
-var ninjas = [];
-ninjas.push('shaun');
-//ninjas.push(20); // error
-//union types
-var mixed = [];
-mixed.push('hello');
-mixed.push(20);
-//mixed.push(false); // error
-console.log(mixed);
-var uid;
-uid = '123';
-uid = 123;
-//uid = false; // error
-//objects
-// this is generic object type
-var ninjaOne;
-ninjaOne = { name: 'yoshi', age: 30 };
-//ninjaOne = 'hello'; // error
-//ninjaOne = 20; // error
-// this is specific object type
-var ninjaTwo;
-ninjaTwo = { name: 'mario', age: 20, beltColor: 'black' };
-//ninjaTwo = { name: 'mario', age: 20 }; // error
-//ninjaTwo = { name: 'mario', age: '20', beltColor: 'black' }; // error
+// any type
+var imranVar;
+//i can replace the value with any type
+//string
+imranVar = "Hello, Imran!";
+console.log(imranVar);
+//number
+imranVar = 42;
+console.log(imranVar);
+//boolean
+imranVar = true;
+console.log(imranVar);
+//any type arrays
+var mixedArray = [];
+//pushing different types of values
+mixedArray.push(10);
+mixedArray.push("TypeScript");
+mixedArray.push(false);
+mixedArray.push({ name: "Imran", age: 25 });
+console.log(mixedArray);
+//any type in objects   
+var imranObject;
+imranObject = { name: "Imran", age: 25 };
+console.log(imranObject);
--- workfiles/sandbox.ts
+++ workfiles/sandbox.ts
@@ -1,54 +1,29 @@
-//explicit types
-let character: string;
-let age: number;
-let isLoggedIn: boolean;
+// any type
+let imranVar: any;
 
-//age = 'luigi' // error
+//i can replace the value with any type
+//string
+imranVar = "Hello, Imran!";
+console.log(imranVar);
+//number
+imranVar = 42;
+console.log(imranVar);
+//boolean
+imranVar = true;
+console.log(imranVar);
 
-age = 30;
+//any type arrays
+let mixedArray: any[] = [];
 
-//character = 20; // error
+//pushing different types of values
+mixedArray.push(10);
+mixedArray.push("TypeScript");
+mixedArray.push(false);
+mixedArray.push({ name: "Imran", age: 25 });
 
-character = 'mario';
+console.log(mixedArray);
 
-//isLoggedIn = 25; // error
-
-isLoggedIn = true;
-
-//arrays
-let ninjas: string[] = [];
-
-ninjas.push('shaun');
-//ninjas.push(20); // error
-
-//union types
-let mixed: (string | number)[] = [];
-    
-mixed.push('hello');
-mixed.push(20);
-//mixed.push(false); // error
-console.log(mixed);
-
-let uid: string | number;
-
-uid = '123';
-uid = 123;
-//uid = false; // error
-
-//objects
-// this is generic object type
-let ninjaOne: object;
-ninjaOne = { name: 'yoshi', age: 30 };
-//ninjaOne = 'hello'; // error
-//ninjaOne = 20; // error
-
-// this is specific object type
-let ninjaTwo: {
-    name: string,
-    age: number,
-    beltColor: string
-};
-
-ninjaTwo = { name: 'mario', age: 20, beltColor: 'black' };
-//ninjaTwo = { name: 'mario', age: 20 }; // error
-//ninjaTwo = { name: 'mario', age: '20', beltColor: 'black' }; // error
+//any type in objects   
+let imranObject: {name: any, age: any};
+imranObject = { name: "Imran", age: 25 };
+console.log(imranObject);
